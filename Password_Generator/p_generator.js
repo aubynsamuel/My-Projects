a3d232f573ad3e747094f3dd62a9560693b3a287
De General <aubynsamuel05@gmail.com>
2025-11-10 04:14:30 +0000

    improve password generation and copying functionality

--- Password_Generator/p_generator.js
+++ Password_Generator/p_generator.js
@@ -1,40 +1,41 @@
-let plength
+let pLength
 let characters = "abcdefghijklmnopqrstuvwxyz!@#$%^&*()-+?/ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-let password = ""
-let index
 
-let Password = () => {
-    for (let i = 0; i < plength; i++) {
-        index = Math.floor(Math.random() * (characters.length))
+const pLengthField = document.getElementById("passwordLength")
+const generateBtn = document.getElementById("generate")
+const passwordDisplay = document.getElementById("password")
+const copyBtn = document.getElementById("copy")
+
+const generatePassword = () => {
+    let password = ""
+    for (let i = 0; i < pLength; i++) {
+        const index = Math.floor(Math.random() * (characters.length))
         password += characters.charAt(index)
     }
     return password
 }
 
-document.getElementById("generate").onclick = function () {
-    password = ""
-
-    if (document.getElementById("passwordLength").value == false) {
-        window.alert("Enter your prefered password length to generate password")
-        document.getElementById("generate").innerHTML = "Generate"
+generateBtn.onclick = () => {
+    if (pLengthField.value == "") {
+        alert("Enter your preferred password length to generate password")
+        generateBtn.innerText = "Generate"
     } else {
-        document.getElementById("generate").innerHTML = "Regenerate"
+        generateBtn.innerText = "Regenerate"
     }
 
-    plength = document.getElementById("passwordLength").value
-    document.getElementById("password").innerText = `Password: ${Password()}`
-
+    pLength = pLengthField.value
+    passwordDisplay.innerText = `Password: ${generatePassword()}`
 }
-document.getElementById("copy").onclick = function () {
-    var copytext = document.getElementById("password")
-    var textarea = document.createElement("textarea")
-    textarea.value = password
+
+copyBtn.onclick = () => {
+    const copyText = passwordDisplay.innerText
+    const textarea = document.createElement("textarea")
+    textarea.value = copyText.replace("Password: ", "")
     document.body.appendChild(textarea)
     textarea.select()
     document.execCommand("copy")
     document.body.removeChild(textarea)
-    alert("Password Copied: " + password)
-
+    alert("Password Copied: " + copyText)
 }
 
 
